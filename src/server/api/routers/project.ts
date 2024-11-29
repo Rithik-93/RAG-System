8627d46d6397f5af55a86f21135d53c0a5e2c3d8
Rithik M <rithik7899@gmail.com>
2024-11-29 22:47:16 +0530

    meeting api

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -88,5 +88,31 @@ export const projectRouter = createTRPCRouter({
                 createdAt: 'desc'
             }
         })
+    }),
+
+    uploadMeeting: protectedProcedure.input(z.object({
+        projectId: z.string(),
+        meetingUrl: z.string(),
+        name: z.string()
+    }))
+        .mutation(async ({ ctx, input }) => {
+            const meeting = await ctx.db.meeting.create({
+                data: {
+                    meetingUrl: input.meetingUrl,
+                    name: input.name,
+                    projectId: input.projectId,
+                    status: 'PROCESSING'
+                }
+            })
+        }),
+
+    getMeetings: protectedProcedure.input(z.object({
+        projectId: z.string()
+    })).query(async ({ ctx, input }) => {
+        return await ctx.db.meeting.findMany({
+            where: {
+                projectId: input.projectId
+            }
+        })
     })
 })
